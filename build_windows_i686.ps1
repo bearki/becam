f7e6d7439f5fb8651544d5b84bf85c112830a7da
bearki <bearki@126.com>
2024-03-20 21:38:50 +0800

    优化一下编译脚本

--- build_windows_i686.ps1
+++ build_windows_i686.ps1
@@ -1,4 +1,4 @@
 & "${PSScriptRoot}\build_windows_any.ps1" `
     -BuildType Release `
     -BuildArch i686 `
-    -Toolchain "C:\MinGW\mingw32"
+    -Toolchain "${Env:MinGW32}"
--- build_windows_x86_64.ps1
+++ build_windows_x86_64.ps1
@@ -1,4 +1,4 @@
 & "${PSScriptRoot}\build_windows_any.ps1" `
     -BuildType Release `
     -BuildArch x86_64 `
-    -Toolchain "C:\MinGW\mingw64"
+    -Toolchain "${Env:MinGW64}"
